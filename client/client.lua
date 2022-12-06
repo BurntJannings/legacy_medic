e97623684d016cca9f01f217b22accac7ce788bb
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2022-12-06 02:33:08 -0700

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -31,6 +31,7 @@ function SetupUsePrompt()
 end
 
 Citizen.CreateThread(function()
+	SetupUsePrompt()
 	while true do
 		Wait(0)
 		local ped = PlayerPedId()
@@ -38,9 +39,9 @@ Citizen.CreateThread(function()
         for k,v in pairs(Doctoroffices) do
             local distance = GetDistanceBetweenCoords(v.Pos.x, v.Pos.y, v.Pos.z, pedpos.x, pedpos.y, pedpos.z, false)
             if distance <= 1.0 then
-				SetupUsePrompt()
-                		local item_name = CreateVarString(10, 'LITERAL_STRING', _U('Open_Cabinet'))
-                		PromptSetActiveGroupThisFrame(PromptGorup, item_name)
+
+                local item_name = CreateVarString(10, 'LITERAL_STRING', _U('Open_Cabinet'))
+                PromptSetActiveGroupThisFrame(PromptGorup, item_name)
 		if Citizen.InvokeNative(0xC92AC953F0A982AE, UsePrompt) then
 			TriggerServerEvent('legacy_medic:checkjob')
 			if CheckTable(MedicJobs,Playerjob) then 
