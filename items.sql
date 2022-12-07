7a997275775e182b824257d42cd5722450d261ca
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2022-12-07 12:49:25 -0700

    Added

---
+++ items.sql
@@ -0,0 +1,2 @@
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Bandage', 'Bandage', 10, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Morphine', 'Morphine', 10, 1, 'item_standard', 0);
