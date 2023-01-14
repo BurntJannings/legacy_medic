1be12c651eac618b618ee3e9b5d8000848a176f6
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2023-01-14 16:19:01 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,7 +3,7 @@ local VORPInv = {}
 
 local VORPInv = exports.vorp_inventory:vorp_inventoryApi()
 
-TriggerEvent("getCore",function(core)
+TriggerEvent("getCore", function(core)
     VorpCore = core
 end)
 
@@ -14,7 +14,7 @@ RegisterServerEvent('legacy_medic:checkjob', function()
     local _source = source
     local Character = VorpCore.getUser(_source).getUsedCharacter
     local job = Character.job
-    TriggerClientEvent('legacy_medic:sendjob',_source, job)
+    TriggerClientEvent('legacy_medic:sendjob', _source, job)
 end)
 
 ---@param table table
@@ -22,11 +22,11 @@ end)
 ---@return boolean
 
 local CheckPlayer = function(table, job)
-    
-    for _, jobholder in pairs (table) do
-    local onduty = exports["syn_society"]:IsPlayerOnDuty(jobholder,job)
-    print(onduty)
-    return onduty
+
+    for _, jobholder in pairs(table) do
+        local onduty = exports["syn_society"]:IsPlayerOnDuty(jobholder, job)
+        print(onduty)
+        return onduty
     end
 
     return false
@@ -34,73 +34,78 @@ end
 
 RegisterServerEvent("legacy_medicalertjobs", function()
     local _source = source
-    local test = true
-                if Config.synsociety then
-                    if CheckPlayer(stafftable,MedicJobs[1]) or CheckPlayer(stafftable, MedicJobs[2]) then
-                        VorpCore.NotifyRightTip(_source,_U('doctoractive'),4000)
-                    else
-                    TriggerClientEvent('legacy_medic:finddoc', _source)
-                    end
-                else
-                    for z, m in ipairs(GetPlayers()) do
-                        local User = VorpCore.getUser(m)
-                        local used = User.getUsedCharacter
-                          if CheckTable(MedicJobs, used.job) then
-                            TriggerClientEvent('legacy_medic:finddoc', _source)
-                          end
-                    end
-                end
-                      
-     end)
-    
-    RegisterServerEvent("legacy_medic:sendPlayers", function(source)
-        local _source = source
-        local user = VorpCore.getUser(_source).getUsedCharacter
-        local job = user.job -- player job
-    
-        if CheckTable(MedicJobs, job) then
-            stafftable[#stafftable + 1] = _source -- id
+    local docs = 0
+    if Config.synsociety then
+        if CheckPlayer(stafftable, MedicJobs[1]) or CheckPlayer(stafftable, MedicJobs[2]) then
+            VorpCore.NotifyRightTip(_source, _U('doctoractive'), 4000)
+        else
+            TriggerClientEvent('legacy_medic:finddoc', _source)
         end
-    end)
+    else
+        for z, m in ipairs(GetPlayers()) do
+            local User = VorpCore.getUser(m)
+            local used = User.getUsedCharacter
+            if CheckTable(MedicJobs, used.job) then
+                docs = docs + 1
+
+            end
+        end
+        if docs < 1 then
+            TriggerClientEvent('legacy_medic:finddoc', _source)
+        end
+
+    end
+
+end)
+
+RegisterServerEvent("legacy_medic:sendPlayers", function(source)
+    local _source = source
+    local user = VorpCore.getUser(_source).getUsedCharacter
+    local job = user.job -- player job
+
+    if CheckTable(MedicJobs, job) then
+        stafftable[#stafftable + 1] = _source -- id
+    end
+end)
 
 AddEventHandler('playerDropped', function()
-        local _source = source        
-        for index, value in pairs (stafftable) do
-            if value == _source then
-            stafftable[index] = nil
-            end
-         end
-    end)
-
-RegisterServerEvent('legacy_medic:takeitem', function(item,number)
     local _source = source
-    VORPInv.addItem(_source, item, number) 
-    VorpCore.NotifyRightTip(_source,_U('Received')..number.. _U('Of') ..item,4000)
+    for index, value in pairs(stafftable) do
+        if value == _source then
+            stafftable[index] = nil
+        end
+    end
+end)
+
+RegisterServerEvent('legacy_medic:takeitem', function(item, number)
+    local _source = source
+    VORPInv.addItem(_source, item, number)
+    VorpCore.NotifyRightTip(_source, _U('Received') .. number .. _U('Of') .. item, 4000)
 
 end)
 
 RegisterServerEvent("legacy_medic:reviveplayer")
 AddEventHandler("legacy_medic:reviveplayer", function()
- 
+
     local _source = source
     local Character = VorpCore.getUser(_source).getUsedCharacter
     local money = Character.money
-if not Config.gonegative then
-    if money >= Config.doctors.amount then
+    if not Config.gonegative then
+        if money >= Config.doctors.amount then
+            Character.removeCurrency(0, Config.doctors.amount) -- Remove money 1000 | 0 = money, 1 = gold, 2 = rol
+            VorpCore.NotifyRightTip(_source, _U('revived') .. Config.doctors.amount, 4000)
+            TriggerClientEvent('legacy_medic:revive', _source)
+        else
+            VorpCore.NotifyRightTip(_source, _U('notenough') .. Config.doctors.amount, 4000)
+        end
+    elseif Config.gonegative then
         Character.removeCurrency(0, Config.doctors.amount) -- Remove money 1000 | 0 = money, 1 = gold, 2 = rol
-        VorpCore.NotifyRightTip(_source,_U('revived')..Config.doctors.amount,4000)
+        VorpCore.NotifyRightTip(_source, _U('revived') .. Config.doctors.amount, 4000)
         TriggerClientEvent('legacy_medic:revive', _source)
-    else
-        VorpCore.NotifyRightTip(_source,_U('notenough')..Config.doctors.amount,4000)
-    end
-elseif Config.gonegative then
-    Character.removeCurrency(0, Config.doctors.amount) -- Remove money 1000 | 0 = money, 1 = gold, 2 = rol
-    VorpCore.NotifyRightTip(_source,_U('revived')..Config.doctors.amount,4000)
-    TriggerClientEvent('legacy_medic:revive', _source)
 
-else
-        VorpCore.NotifyRightTip(_source,_U('notenough')..Config.doctors.amount,4000)
-end
+    else
+        VorpCore.NotifyRightTip(_source, _U('notenough') .. Config.doctors.amount, 4000)
+    end
 end)
 
 RegisterServerEvent('legacy_medic:reviveclosestplayer')
@@ -108,19 +113,20 @@ AddEventHandler('legacy_medic:reviveclosestplayer', function(closestPlayer)
     local _source = source
     local Character = VorpCore.getUser(_source).getUsedCharacter
     local target = VorpCore.getUser(closestPlayer).getUsedCharacter
-    local playname2 = target.firstname.. ' ' ..target.lastname
+    local playname2 = target.firstname .. ' ' .. target.lastname
     local count = VORPInv.getItemCount(_source, Config.Revive)
-    local playername = Character.firstname.. ' ' ..Character.lastname
+    local playername = Character.firstname .. ' ' .. Character.lastname
 
     if count > 0 then
         VORPInv.subItem(_source, Config.Revive, 1)
         TriggerClientEvent('legacy_medic:revive', closestPlayer)
         if Config.usewebhook then
-        VorpCore.AddWebhook(Config.WebhookTitle, Config.Webhook,  _U('Player_Syringe') ..playername.. _U('Used_Syringe') ..playname2)
+            VorpCore.AddWebhook(Config.WebhookTitle, Config.Webhook,
+                _U('Player_Syringe') .. playername .. _U('Used_Syringe') .. playname2)
         else
         end
     else
-        VorpCore.NotifyRightTip(_source, _U('Missing') ..Config.Revive,4000)
+        VorpCore.NotifyRightTip(_source, _U('Missing') .. Config.Revive, 4000)
     end
 end)
 
@@ -133,25 +139,25 @@ AddEventHandler('legacy_medic:healplayer', function(closestPlayer)
         VORPInv.subItem(_source, Config.Bandage, 1)
         TriggerClientEvent('vorp:heal', closestPlayer)
     else
-        VorpCore.NotifyRightTip(_source, _U('Missing') ..Config.Bandage,4000)
+        VorpCore.NotifyRightTip(_source, _U('Missing') .. Config.Bandage, 4000)
     end
 end)
 
 VORPInv.RegisterUsableItem(Config.Revive, function(data)
-    TriggerClientEvent('legacy_medic:getclosestplayerrevive',data.source)
-    VorpCore.NotifyRightTip(data.source, "You used " ..Config.Revive,4000)
-  end)
-  
-  VORPInv.RegisterUsableItem(Config.Bandage, function(data)
-    TriggerClientEvent('legacy_medic:getclosestplayerbandage',data.source)
-    VorpCore.NotifyRightTip(data.source, "You used " ..Config.Bandage,4000)
-  end)
+    TriggerClientEvent('legacy_medic:getclosestplayerrevive', data.source)
+    VorpCore.NotifyRightTip(data.source, "You used " .. Config.Revive, 4000)
+end)
 
-  function CheckTable(table, element)
+VORPInv.RegisterUsableItem(Config.Bandage, function(data)
+    TriggerClientEvent('legacy_medic:getclosestplayerbandage', data.source)
+    VorpCore.NotifyRightTip(data.source, "You used " .. Config.Bandage, 4000)
+end)
+
+function CheckTable(table, element)
     for k, v in pairs(table) do
         if v == element then
             return true
         end
     end
     return false
-  end
+end
